14c2989e509d046593c754c2aec37b600634ea0a
Pedro <43250725+PTracana@users.noreply.github.com>
2021-05-18 22:22:52 +0100

    minor updates
    still not fixed trying to find the fix, meanwhile some minor visibility fixes

--- Rl.sql
+++ Rl.sql
@@ -3,10 +3,10 @@ DROP TRIGGER IF EXISTS supplier_sec_verify on supplies_sec ;
 CREATE OR REPLACE FUNCTION supplier_sec_verify_proc()
     RETURNS TRIGGER AS $$
     BEGIN
-        IF ((SELECT count(*) from supplies_sec as a WHERE a.ean = new.ean) >= 3) THEN
-            RAISE EXCEPTION 'limite de suppliers ';
-        END IF;
-        RETURN new;
+    IF ((SELECT count(*) from supplies_sec as a WHERE a.ean = new.ean) >= 3) THEN
+        RAISE EXCEPTION 'limite de suppliers para produto %', new.ean;
+    END IF;
+    RETURN new;
     END
     $$ LANGUAGE plpgsql;
 
