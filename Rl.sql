b15665a546624e7c9f58152ae30d6719280938d8
Pedro <43250725+PTracana@users.noreply.github.com>
2021-05-19 00:02:44 +0100

    Update Rl.sql

--- Rl.sql
+++ Rl.sql
@@ -3,7 +3,7 @@ DROP TRIGGER IF EXISTS supplier_sec_verify on supplies_sec ;
 CREATE OR REPLACE FUNCTION supplier_sec_verify_proc()
     RETURNS TRIGGER AS $$
     BEGIN
-    IF ((SELECT count(*) from supplies_sec as a WHERE a.ean = new.ean) >= 3) THEN
+    IF ((SELECT count(*) from supplies_sec as a WHERE new.ean = a.ean) >= 3) THEN
         RAISE EXCEPTION 'limite de suppliers para produto %', new.ean;
     END IF;
     RETURN new;
@@ -11,6 +11,6 @@ CREATE OR REPLACE FUNCTION supplier_sec_verify_proc()
     $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER supplier_sec_verify
-    BEFORE UPDATE OR INSERT on supplies_sec
+    BEFORE INSERT OR UPDATE on supplies_sec
     EXECUTE PROCEDURE supplier_sec_verify_proc();
 
